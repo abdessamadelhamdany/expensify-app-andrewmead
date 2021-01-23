c8edb3426f3a7901f7fa0de2dd1f24fa25cd4b61
Abdessamad El Hamdany <abdessamadelhamdany@gmail.com>
2021-01-23 12:56:04 +0100

    Add expensify redux store

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
-const EditExpensePage = () => <div>This is edit expense page</div>;
+const EditExpensePage = (props) => {
+    console.log(props);
+    return <div>This is edit expense page {props.match.params.id}</div>;
+};
 
 export default EditExpensePage;
