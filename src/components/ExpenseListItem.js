5d03f4a47d748d1e28eb8de83b8c2f3154438878
Abdessamad El Hamdany <abdessamadelhamdany@gmail.com>
2021-01-27 15:17:49 +0100

    Move remove button to editExpense page

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,24 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { connect } from "react-redux";
-import { removeExpense } from "../actions/expenses";
-// console.log(now.format("MMM Do, YYYY"));
+import moment from "moment";
 
-const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
+const ExpenseListItem = ({ id, description, amount, createdAt }) => (
   <div>
-    <h3>{description}</h3>
+    <Link to={`/edit/${id}`}>
+      <h3>{description}</h3>
+    </Link>
     <p>
-      {amount} - {createdAt}
+      {amount} - {moment(createdAt).format("MMM Do, YYYY")}
     </p>
-    <Link to={`/edit/${id}`}>Edit</Link>
-    <button
-      onClick={() => {
-        dispatch(removeExpense({ id }));
-      }}
-    >
-      Remove
-    </button>
   </div>
 );
 
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
